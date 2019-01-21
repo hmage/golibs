b483c97f33377bca755a59a58559dee701c4f4b8
Andrey Meshkov <ay.meshkov@gmail.com>
2019-01-21 14:24:42 +0300

    Add TraceToStderr flag

--- log/log.go
+++ log/log.go
@@ -1,16 +1,22 @@
-// Wrapper for standard library log, with the only difference is that it has extra function Tracef() and optional verbose flag to enable output from that.
+// Package log is a wrapper for standard library log, with the only difference is that it has extra function Tracef() and optional verbose flag to enable output from that.
 package log
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"path"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
+// Verbose flag enables or disables output from log.Tracef
 var Verbose = false
+
+// TraceToStderr controls the log.Tracef output. If true - prints it to stderr. Otherwise, it uses log.Print.
+var TraceToStderr = false
 
 // Print calls Output to print to the standard logger.
 // Arguments are handled in the manner of fmt.Print.
@@ -40,18 +46,33 @@ func Fatalf(format string, v ...interface{}) {
 	log.Fatalf(format, v...)
 }
 
+// Tracef prints additional trace information
 func Tracef(format string, v ...interface{}) {
 	if Verbose {
 		pc := make([]uintptr, 10) // at least 1 entry needed
 		runtime.Callers(2, pc)
 		f := runtime.FuncForPC(pc[0])
 		var buf strings.Builder
-		buf.WriteString(fmt.Sprintf("%s(): ", path.Base(f.Name())))
+		buf.WriteString(fmt.Sprintf("[%d] %s(): ", getGID(), path.Base(f.Name())))
 		text := fmt.Sprintf(format, v...)
 		buf.WriteString(text)
 		if len(text) == 0 || text[len(text)-1] != '\n' {
 			buf.WriteRune('\n')
 		}
-		fmt.Fprint(os.Stderr, buf.String())
+		if TraceToStderr {
+			fmt.Fprint(os.Stderr, buf.String())
+		} else {
+			log.Print(buf.String())
+		}
 	}
+}
+
+// getGID gets goroutine ID (see the details here: https://blog.sgmansfield.com/2015/12/goroutine-ids/)
+func getGID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
 }
